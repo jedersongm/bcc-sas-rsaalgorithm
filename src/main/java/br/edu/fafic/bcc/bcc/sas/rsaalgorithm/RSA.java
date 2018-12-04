659faa13e806122f1768363bcc05a9f3f1ddcb95
Jéderson Gonçalves Martins <jedersongmoura@gmail.com>
2018-12-03 21:43:00 -0300

    Classe RSA

---
+++ src/main/java/br/edu/fafic/bcc/bcc/sas/rsaalgorithm/RSA.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.fafic.bcc.bcc.sas.rsaalgorithm;
+
+import java.math.BigInteger;
+import java.util.Random;
+
+/**
+ *
+ * @author jederson
+ */
+public class RSA {
+    
+    private BigInteger p;
+    private BigInteger q;
+    private BigInteger n;
+    private BigInteger z;
+    private BigInteger e;
+    private BigInteger d;
+    private final int bitlength = 1024;
+    private Random     r;
+    
+    
+}
